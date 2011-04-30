a5ba1c22f18326f0297c5b51e22753335db3826d
gugahoi <guga@maurorisch.com.br>
2011-04-30 11:28:22 +1000

    Remade jDownloader widget without simpleXML objects.

--- widgets/wjDownloader.php
+++ widgets/wjDownloader.php
@@ -28,8 +28,9 @@ JDHEADER;
 }
 
 function widgetjDownloader(){
-	global $jd_url;
-	$jd = $jd_url;
+	//global $jd_url;
+	//$jd = $jd_url;
+	$jd = "http://localhost:10025/";
 
 	try{
 		$speed   = @file_get_contents($jd."get/speed");
@@ -38,27 +39,20 @@ function widgetjDownloader(){
 		$speedLimit = @file_get_contents($jd."get/speedlimit");
 
 		$dlCount  = @file_get_contents($jd."get/downloads/currentcount");
-		$dlList  =  new SimpleXMLElement(@file_get_contents($jd."get/downloads/currentlist"));
+		$dlList  =  @file_get_contents($jd."get/downloads/alllist");
 
-		$finished  = new SimpleXMLElement (@file_get_contents($jd."get/downloads/finishedlist"));
-		$finishedCount = @file_get_contents($jd."get/downloads/finishedcount");
+		$dlList = str_replace("</jdownloader>", "", $dlList);
+		$dlList = str_replace("<jdownloader>", "", $dlList);
+		
+		$dlList = str_replace("<package ", "|" , $dlList);
+		$dlList = str_replace(">\n<file", "" , $dlList);
+		$dlList = str_replace("/>\n</package>", "" , $dlList);
+		$dlList = str_replace("/>", "" , $dlList);
+		$dlList = str_replace("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n\n", "" , $dlList);
+		$dlList = str_replace("\"", "" , $dlList);
 
-		$allCount = @file_get_contents($jd."get/downloads/allcount");
-		$allList = new SimpleXMLElement(@file_get_contents($jd."get/downloads/alllist"));
-
-		$dllimi = @file_get_contents($jd."get/speedlimit");
-		$dllimit = @file_get_contents($jd."get/speedlimit");
-
-		$setdllimit = $jd."action/set/download/limit/";
-
-
-		//echo "<pre>";
-		//print_r($allList);
-		//echo "</pre>";
-
-
-
-		echo "<input type='text'  class='btnDown' style='float: right;' value='".$speedLimit."' id='dllimit' size='1' />";
+	
+		$dlList = explode("|",$dlList);
 
 		$action="";
 
@@ -77,158 +71,117 @@ function widgetjDownloader(){
 		}
 
 		echo "<a href='".$action."' target='nothing'>".$status."</a>";
+
 		echo "<table border=\"0\" width='100%' style='table-layout:fixed;' cellspacing='0' cellpadding='0'><tr>";
 		echo "<th width='10%'></th>";
-		echo "<th width='40%'></th>";
-		echo "<th width='50%'></th>";
+		echo "<th width='40%'>Name</th>";
+		echo "<th width='50%'>Status</th>";
 		echo "</tr>";
-
-		foreach($allList as $item){
-			$eta    = $item->attributes()->package_ETA;
-			$links_progress = $item->attributes()->package_linksinprogress;
-			$links_total = $item->attributes()->package_linkstotal;
-			$downloaded  = $item->attributes()->package_loaded;
-			$total_size  = $item->attributes()->package_size;
-			$name     = $item->attributes()->package_name;
-			$percent  = $item->attributes()->package_percent;
-			$speed    = $item->attributes()->package_speed;
-			$to_do    = $item->attributes()->package_todo;
-
-			$popup  = "";
-			$popup .= "<p>Name: ".$name."</p>";
-			$popup .= "<p>Links: ".$links_progress." of ".$links_total."</p>";
-			$popup .= "<p>Progress: ".$percent."% (".$downloaded."/".$total_size.")</p>";
 			
-			$color = "red";
+		foreach($dlList as $item){
+	 		if($item!=""){
+		 		$item = str_replace("\n", "" , $item);
+		 		$item = str_replace(" package", "|package" , $item);
+		 		$item = str_replace("file_", "|file" , $item);					
+				
+				$original = $item;
 
-			if($links_progress!=0){
-				$hoster = $item->file->attributes()->file_hoster;
-				$to_do	= $item->attributes()->package_todo;
-				$popup .= "<p>Hoster: ".$hoster."</p>";
-				$color  = "green";
-			}
+				$item = explode("|",$item);
+				//echo "<pre>";
+				//print_r($item);
+				//echo "</pre>";
+				
+				
+				
+				$eta    	 = substr($item[0],12);
+				$links_progress = substr($item[1],24);
+				$links_total = substr($item[2],19);
+				$downloaded  = substr($item[3],15);
+				$name		 = substr($item[4],13);
+				$percent 	 = substr($item[5],16);
+				$total_size  = substr($item[6],13);
+				$speed   	 = substr($item[7],14);
+				$to_do   	 = substr($item[8],13);
+
+				$popup  = "";
+				$popup .= "<p>Name: ".$name."</p>";
+				$popup .= "<p>Active Links: ".$links_progress." of ".$links_total."</p>";
+				$popup .= "<p>Progress: ".$percent."% (".$downloaded."/".$total_size.")</p>";
+				
+				$color = "red";
+	
+				if($links_progress!=0){
+					$hoster = substr($item[9],11);
+					//$to_do	= $item->attributes()->package_todo;
+					$popup .= "<p>Hoster: ".$hoster."</p>";
+					$color  = "green";
+				}
+				
+				$more = ($links_total>1)?"<img width='10px' src='media/btnAdd.png' style:'vertical-align:middle;' onClick=\"toggle('".$name."');\" />":"";
+
+				//echo "$eta $links_progress $links_total $downloaded $name $percent $total_size $speed $to_do";
 
 
+				echo "<tr>";
+				echo "<td>".$more."</td>";
+				echo "<td><font color=".$color."><div style='text-overflow:ellipsis;overflow:hidden; white-space:nowrap;' onMouseOver=\"ShowPopupBox('".$popup."');\" onMouseOut=\"HidePopupBox();\">".$name."</div></font></td>";
+				echo "<td><div class=\"queueitem\">";
+				echo "\t\t\t\t<div class=\"progressbar\">";
+				echo "\t\t\t\t\t<div class=\"progress\" style=\"width:".$percent."%\"></div>";
+				echo "\t\t\t\t\t<div class=\"progresslabel\" style='text-align: center;'>".$to_do." left @ ".$speed." - ".$eta."</div>";
+				echo "\t\t\t\t</div><!-- .progressbar -->";
+				echo "\t\t\t</div><!-- .queueitem -->";
+				echo "</td>";
+				echo "</tr>";
 
-			echo "<tr>";
-			echo "<td><img width='10px' src='media/btnAdd.png' style:'vertical-align:middle;' onClick=\"toggle('".$name."');\" /></td>";
-			echo "<td><font color=".$color."><div style='text-overflow:ellipsis;overflow:hidden; white-space:nowrap;' onMouseOver=\"ShowPopupBox('".$popup."');\" onMouseOut=\"HidePopupBox();\">".$name."</div></font></td>";
-			echo "<td><div class=\"queueitem\">";
-			echo "\t\t\t\t<div class=\"progressbar\">";
-			echo "\t\t\t\t\t<div class=\"progress\" style=\"width:".$percent."%\"></div>";
-			echo "\t\t\t\t\t<div class=\"progresslabel\" style='text-align: center;'>".$to_do." left @ ".$speed." - ".$eta."</div>";
-			echo "\t\t\t\t</div><!-- .progressbar -->";
-			echo "\t\t\t</div><!-- .queueitem -->";
-			echo "</td>";
-			echo "</tr>";
+				$subitem = explode("filename=",$original);
+				
+/*
+				echo "<pre>";
+				print_r($subitem);
+				echo "</pre>";
+*/
 
-
-			if(sizeof($item->file)>1){
-				foreach($item->file as $subitem){
-
-					$subitem  = $subitem->attributes();
-					$subname  = $subitem->file_name;
-					$subpercent = $subitem->file_percent;
-					$substatus = $subitem->file_status;
-					$subhoster = $subitem->file_hoster;
-
-					$subcolor = (!empty($substatus))?"green":"red";
-
+				for($x=1; $x<sizeof($subitem);$x+=1){
+					
+					$s = explode("|",$subitem[$x]);
+					
+					$subname 	= substr($s[0],0);
+					$subperc 	= floor(substr($s[2],12));
+					$subspeed	= substr($s[3],10);
+					$substatus	= substr($s[4],11);
+					$subhoster 	= (!empty($substatus))?substr($s[5],11):"";
+					$subcolor 	= (!empty($substatus) && $substatus!="/ ")?"green":"red";
+					
+					$substatus = ($subperc>=100)?"Finished":$substatus;
+					
 					echo "<tr style='display:none;' name='".$name."'>";
-					echo "<td></td>";
-					echo "<td><font color=".$color."><div style='text-overflow:ellipsis;overflow:hidden; white-space: nowrap;'>".$subname."</div></font></td>";
+					echo "<td>$x</td>";
+					echo "<td><font color=".$subcolor."><div style='text-overflow:ellipsis;overflow:hidden; white-space:nowrap;' >".$subname."</div></font></td>";
 					echo "<td><div class=\"queueitem\">";
 					echo "\t\t\t\t<div class=\"progressbar\">";
-					echo "\t\t\t\t\t<div class=\"progress\" style=\"width:".$subpercent."%\"></div>";
-					echo "\t\t\t\t\t<div class=\"progresslabel\" style='text-align: center;'>".$substatus."</div>";
+					echo "\t\t\t\t\t<div class=\"progress\" style=\"width:".$subperc."%\"></div>";
+					echo "\t\t\t\t\t<div class=\"progresslabel\" style='text-align: center;'>$substatus</div>";
 					echo "\t\t\t\t</div><!-- .progressbar -->";
 					echo "\t\t\t</div><!-- .queueitem -->";
 					echo "</td>";
 					echo "</tr>";
+
 				}
-
 			}
-				/*
-				echo "<pre>";
-	 			print_r(sizeof($item->file));
-	 			echo "</pre>";
-				*/
-
 		}
-		echo "</table>\n";
+		echo "</table>";
+	
+		//$dlList = explode("package",$dlList);
+		//$dlList = explode("file",$dlList);
+
+		//print_r($dlList);
+		
 	}
 	catch(Exception $e){
-		echo "Ooops, something went wrong! Is jDownloader open? Check <a href='".$jd."help'>HERE</a>";
+		echo "Error: $e";
 	}
 }
-/*
-JDRemoteControl 9568
-
-Usage:
-
-1)Replace %X% with your value
-Sample: /action/save/container/C:\backup.dlc
-2)Replace (true|false) with true or false
-
-Get Values:
-
-/get/speed						Get current Speed
-/get/ip							Get IP
-/get/randomip					Answers with Random IP as replacement for real IP-Check
-/get/config	Get 				Config
-/get/version					Get Version
-/get/rcversion					Get RemoteControl Version
-/get/speedlimit					Get current Speedlimit
-/get/isreconnect				Get If Reconnect
-/get/downloadstatus				Get Downloadstatus
-								Values: RUNNING, NOT_RUNNING, STOPPING
-/get/downloads/currentcount		Get amount of current downloads
-/get/downloads/currentlist		Get Current Downloads List (XML)
-/get/downloads/allcount			Get amount of downloads in list
-/get/downloads/alllist			Get list of downloads in list (XML)
-/get/downloads/finishedcount	Get amount of finished Downloads
-/get/downloads/finishedlist		Get finished Downloads List (XML)
-
-Actions:
-
-/action/start					Start DLs
-/action/pause					Pause DLs
-/action/stop					Stop DLs
-/action/toggle					Toggle DLs
-/action/update/force(0|1)/		Do Webupdate
-									force1 activates auto-restart if update is possible
-/action/reconnect				Do Reconnect
-/action/restart					Restart JD
-/action/shutdown				Shutdown JD
-/action/set/download/limit/%X%	Set Downloadspeedlimit %X%
-/action/set/download/max/%X%	Set max sim. Downloads %X%
-
-/action/add/links/grabber(0|1)/start(0|1)/%X%	Add Links %X% to Grabber
-												Optional:
-												grabber(0|1): Hide/Show LinkGrabber
-												grabber(0|1)/start(0|1): Hide/Show LinkGrabber and start/don't start downloads afterwards
-
-												Sample:
-												/action/add/links/grabber0/start1/http://tinyurl.com/6o73eq http://tinyurl.com/4khvhn
-												Don't forget Space between Links!
-
-/action/add/container/grabber(0|1)/start(0|1)/%X%	Add Container %X%
-													Optional:
-													grabber(0|1): Hide/Show LinkGrabber
-													grabber(0|1)/start(0|1): Hide/Show LinkGrabber and start/don't start downloads afterwards
-
-													Sample:
-													/action/add/container/grabber0/start1/C:\container.dlc
-
-/action/save/container/%X%		Save DLC-Container with all Links to %X%
-								Sample see /action/add/container/%X%
-
-/action/set/reconnectenabled/(true|false)	Set Reconnect enabled or not
-/action/set/premiumenabled/(true|false)		Set Use Premium enabled or not
-*/
-
-
-
 
 if(!empty($_GET['style']) && ($_GET['style'] == "w")) {
 	require_once "../config.php";
