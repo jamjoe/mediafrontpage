713a7fddc674ffaac1eadc4eb738067038e7e9e9
Gustavo Hoirisch <guga@maurorisch.com.br>
2011-05-15 00:40:48 -0700

    Reworked wMessage to have user feedback and work with multiple XBMC instances.

--- widgets/wMessage.php
+++ widgets/wMessage.php
@@ -1,17 +1,83 @@
 <?php
-$wdgtMessage = array("name" => "Message XBMC", "type" => "inline", "function" => "widgetMessage();");
-$wIndex["wMessage"] = $wdgtMessage;
+$wIndex["wMessage"] = array("name" => "Message XBMC", "type" => "inline", "function" => "widgetMessage();", "headerfunction" => "widgetNotificationHeader();");
 
-		if($_SERVER['REQUEST_METHOD'] == "POST"){
-		if(!empty($_POST['keyword'])){
-			$wMessage = 'http://xbmclive:8082/xbmcCmds/xbmcHttp?command=ExecBuiltIn(Notification(NOTICE!,' . $_POST['keyword'] . ',20000))';
-			header("Location: " . $wMessage);
-		exit;
+function widgetNotificationHeader(){
+	echo <<< RSSHEADER
+<script type="text/javascript" language="javascript">
+	<!--
+	function sendMessage(){
+		var xbmc = document.getElementById('xbmc').value;
+		var msg  = document.getElementById('keyword').value;
+		xbmcAjax(xbmc, msg);
 	}
-}
+	function xbmcAjax(url, msg) {
+			//alert(msg);
+			if (window.XMLHttpRequest) {
+				// code for IE7+, Firefox, Chrome, Opera, Safari
+				xmlhttp=new XMLHttpRequest();
+			} else {
+				// code for IE6, IE5
+				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.open("GET", "widgets/wMessage.php?msg="+msg+"&url="+url, true);
 
+			xmlhttp.onreadystatechange = function() {//Call a function when the state changes.
+				if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+					//alert(xmlhttp.responseText);
+					if(xmlhttp.responseText){
+						alert('Message sent successfully');
+					}
+					else{
+						alert('Failed sending message. Error: ' + xmlhttp.responseText);
+					}
+				}
+			}
+			xmlhttp.send();
+		}
+	-->
+</script>
+
+RSSHEADER;
+}
 function widgetMessage() {
-		$self_post = $_SERVER['PHP_SELF'];
-	echo "<form method='post' action='".$self_post."'>Send <b>Notice!</b> <input type='text' style='border:0px; background : #3D3D3D; color:white' name='keyword' id='keyword' />to XBMC.<input type='submit' value='Send'></form>";
+	require_once "config.php";
+	global $xbmcimgpath,$xbmcMessages;
+
+	echo "Send <b>Notice!</b> <input type='text' style='border:0px; background : #3D3D3D; color:white' name='keyword' id='keyword' />to XBMC.
+	<select id='xbmc'>";
+	if(!empty($xbmcMessages)){
+		foreach($xbmcMessages as $title => $link){
+			echo "<option value='".$link."'>".$title."</option>";	
+		}
+	}
+	else{
+		echo "<option value='".substr($xbmcimgpath, 0, strlen($xbmcimgpath)-4)."'>Default</option>";
+	}
+	echo "</select>
+	<input type='button' value='Send' onclick='sendMessage()'>
+	<div id='responseMessage'></div>";
+}
+if(!empty($_GET['msg']) && !empty($_GET['url']))
+{
+/*
+	echo "<p>".$_GET['msg']."</p>";
+	echo "<p>".$_GET['url']."</p>";
+*/
+	$url = $_GET['url']."xbmcCmds/xbmcHttp?command=ExecBuiltIn(Notification(MediaFrontPage,".urlencode($_GET['msg']).",20000))";
+		
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPGET, 1);
+	curl_setopt($ch, CURLOPT_URL, $url);
+
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+	if(strstr($response, 'OK')){
+		echo true;
+	}
+	else{
+		echo $response;
+	}
 }
 ?>
