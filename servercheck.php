905272d8aeff8419a5a766ef58cd00fa5ac39073
gugahoi <guga@maurorisch.com.br>
2011-06-07 11:57:41 +1000

    Applied MFP's style to servercheck.

--- servercheck.php
+++ servercheck.php
@@ -6,23 +6,46 @@ function redirect(){
 	window.location = 'index.php';
 }
 </script>
+<link href="css/front.css" rel="stylesheet" type="text/css" />	
 </head>
 <body>
+<h1>Welcome to Media Front Page.</h1>
 <center>
-If you have no text below, your PHP is not working.<BR>
+<?php if(false){
+?>
+If you have no text below, your PHP is not working.
 <?php
+}
+else{}
 $redirect = true;
-echo "If you can read this, <B>PHP is working.</B><BR>";
+echo "<p>If you can read this, <B>PHP is working.</B></p>";
 $version = phpversion();
-echo "PHP Version $version";
-echo '<br>';
-if(extension_loaded('libxml')) { echo "Lib XML found <img src='media/green-tick.png' height='15px'/>";}else{echo "Lib XML NOT found, you will need to change your php.ini <img src='media/red-cross.png' height='15px'/>"; $redirect = false;}echo '<br>';
-if(extension_loaded('curl')) { echo "Curl found <img src='media/green-tick.png' height='15px'/>";} else { echo "Curl NOT found, you will need to change your php.ini <img src='media/red-cross.png' height='15px'/>"; $redirect = false;} echo '<br>';
-if (file_exists('config.php')){echo "You created config.php, seems like everything is in working order. <img src='media/green-tick.png' height='15px'/>";}
-else if(file_exists('default-config-new.php')){echo "Please rename default-config.php to config.php <img src='media/red-cross.png' height='15px'/>";$redirect = false;}	
-else{echo "It seems you have no config file! Please make sure its named config.php or re-download from <a href=\"https://github.com/MediaFrontPage/mediafrontpage\">Media Front Page</a> <img src='media/red-cross.png' height='15px'/>";$redirect = false;}echo "<br>";
+echo "<p>PHP Version $version</p>";
+echo '<table border="1">';
+if(extension_loaded('libxml')){ 
+	echo "<tr><td>Lib XML found</td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
+}else{
+	echo "<tr><td>Lib XML NOT found, you will need to change your php.ini </td><td><img src='media/red-cross.png' height='15px'/></td></tr>"; 
+	$redirect = false;
+}
+if(extension_loaded('curl')){ 
+	echo "<tr><td>cURL found </td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
+}else{ 
+	echo "<tr><td>cURL <b>NOT</b> found</td><td><img src='media/red-cross.png' height='15px'/></td></tr>"; 
+	$redirect = false;
+}
+if (file_exists('config.php')){
+	echo "<tr><td>You created config.php, seems like everything is in working order. </td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
+} else if(file_exists('default-config-new.php')){
+	echo "<tr><td>Please rename default-config.php to config.php </td><td><img src='media/red-cross.png' height='15px'/></td></tr>";
+	$redirect = false;
+}else{
+	echo "<tr><td>It seems you have no config file! Please make sure its named config.php or re-download from <a href=\"https://github.com/MediaFrontPage/mediafrontpage\">Media Front Page</a> </td><td><img src='media/red-cross.png' height='15px'/></td></tr>";
+	$redirect = false;
+}
+echo '</table>';
 if($redirect){
-	echo "Congratulations! Redirecting to MediaFrontPage in 5 seconds.";
+	echo "<p>Congratulations! Redirecting to MediaFrontPage in 5 seconds.</p>";
 	echo "<script>setTimeout('redirect()', 5000);</script>";
 	if (file_exists('firstrun.php')){unlink('firstrun.php');}
 }
