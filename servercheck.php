d368b2403be8086b6b5d958e661dcd71193970a2
Gustavo Hoirisch <guga@maurorisch.com.br>
2011-06-10 05:38:57 -0700

    Modified servercheck.php with toggling config editor.

--- servercheck.php
+++ servercheck.php
@@ -5,6 +5,14 @@
 function redirect(){
 	window.location = 'index.php';
 }
+function toggle(div){
+	if(document.getElementById(div).style.display == 'inline'){
+		document.getElementById(div).style.display = 'none';
+	}
+	else{
+		document.getElementById(div).style.display = 'inline';
+	}
+}
 </script>
 <link href="css/front.css" rel="stylesheet" type="text/css" />
 <style type="text/css">
@@ -47,50 +55,57 @@ function redirect(){
 else{}
 $redirect = true;
 $version = phpversion();
- if(false){
+if(false){
 ?>
   If you have no text below, your PHP is not working.
-  <?php
+<?php
 }
 else{}
 $redirect = true;
 $version = phpversion();
 
 echo "<tr><td>PHP Version $version</td><td>";if($version > 5){echo "<img src='media/green-tick.png' height='15px'/>";}else{echo "<img src='media/red-cross.png' height='15px'/>";$redirect = false;} echo "</td></tr>";
-if(extension_loaded('libxml')){ 
+if(extension_loaded('libxml')){
 	echo "<tr><td>LibXML found</td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
 }else{
-	echo "<tr><td>LibXML <b>NOT</b> found</td><td><img src='media/red-cross.png' height='15px'/></td></tr>"; 
+	echo "<tr><td>LibXML <b>NOT</b> found</td><td><img src='media/red-cross.png' height='15px'/></td></tr>";
 	$redirect = false;
 }
-if(extension_loaded('curl')){ 
+if(extension_loaded('curl')){
 	echo "<tr><td>cURL found </td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
-}else{ 
-	echo "<tr><td>cURL <b>NOT</b> found</td><td><img src='media/red-cross.png' height='15px'/></td></tr>"; 
+}else{
+	echo "<tr><td>cURL <b>NOT</b> found</td><td><img src='media/red-cross.png' height='15px'/></td></tr>";
 	$redirect = false;
 }
-if (file_exists('default-config.php')){
-	echo "<tr><td>default-config.php found. </td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
+if (file_exists('config.php')){
+	echo "<tr><td><input type='button' value='config.php' onclick=\"toggle('config'); \"/> found. </td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
 }else{
-	echo "<tr><td>default-config.php <b>NOT</b> found</td><td><img src='media/red-cross.png' height='15px'/></td></tr>";
+	echo "<tr><td><input type='button' value='config.php' onclick=\"toggle('config'); \"/>  <b>NOT</b> found";
+	if(file_exists('default-config.php')){
+	}
+	echo "</td><td><img src='media/red-cross.png' height='15px'/></td></tr>";
 	$redirect = false;
 }
 if (file_exists('layout.php')){
+	$valid = true;
 	echo "<tr><td>layout.php found";
 	if(!is_writable('layout.php')){
 		if(@chmod("layout.php", 0777)){
-			echo " and CHMODDED</td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
+			echo " and CHMODDED";
 		}
 		else{
-			echo ", could not be written. Please CHMOD it.</td><td><img src='media/red-cross.png' height='15px'/></td></tr>";
+			echo ", could not be written. Please CHMOD it.";
 			$redirect = false;
+			$valid = false;
 		}
 	}
 	else{
-		echo ", writeable<br><br></td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
+		echo ", writeable";
 	}
+	echo ($valid)?"</td><td><img src='media/green-tick.png' height='15px'/></td></tr>":"</td><td><img src='media/red-cross.png' height='15px'/></td></tr>";
 }else{
 	echo '<tr><td>default-layout.php';
+	$valid = true;
 	if(file_exists("default-layout.php")){
 		if(rename("default-layout.php", "layout.php")){
 			echo " renamed successfully<br><br>";
@@ -99,57 +114,93 @@ if (file_exists('layout.php')){
 	else{
 		echo " could not be found<br>";
 		$redirect = false;
-	}	
+		$valid = false;
+	}
 	if(file_exists("layout.php")){
 		if(!is_writable('layout.php')){
 			if(@chmod("layout.php", 0777)){
-				echo " and CHMODDED</td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
+				echo " and CHMODDED";
 			}
 			else{
-				echo ", could not be written. Please CHMOD it.<br><br></td><td><img src='media/red-cross.png' height='15px'/></td></tr>";
+				echo ", could not be written. Please CHMOD it.";
 				$redirect = false;
+				$valid = false;
 			}
 		}
 		else{
-			echo ", writeable</td><td><img src='media/green-tick.png' height='15px'/></td></tr>";
+			echo ", writeable";
 		}
 	}
+	echo ($valid)?"</td><td><img src='media/green-tick.png' height='15px'/></td></tr>":"</td><td><img src='media/red-cross.png' height='15px'/></td></tr>";
 }
 echo '</table>';
 if($redirect){
-	echo "<p><b>Congratulations! <br>MediaFrontPage One-Time Configuration below.<br><br></b></p>";
-$loadcontent = "default-config.php"; 
-    if($_POST['save_file']) { 
-        $savecontent = stripslashes($_POST['savecontent']); 
-        $fp = @fopen($loadcontent, "w"); 
-        if ($fp) { 
-            fwrite($fp, $savecontent); 
-            fclose($fp);
-print '<a href='.$_SERVER[PHP_SELF].'>Refresh</a>'; 
-print "<html><head><META http-equiv=\"refresh\" content=\"0;URL=$_SERVER[PHP_SELF]\"></head><body>"; 
- 
-} 
-} 
-    $fp = @fopen($loadcontent, "r"); 
-        $loadcontent = fread($fp, filesize($loadcontent)); 
-$lines = explode("\n", $loadcontent);
-$count = count($lines);
-        $loadcontent = htmlspecialchars($loadcontent); 
-        fclose($fp); 
-for ($a = 1; $a < $count+1; $a++) {
-$line .= "$a\n";
+	//echo "<script>setTimeout('redirect()', 5000);</script>";
+	echo "<p>Congratulations! Everything seems to be in working order.</p>";
+	echo "<p><input type='button' onclick=\"window.location = 'index.php';\" value='CONTINUE' /></p>";
+	if (file_exists('firstrun.php')){
+		unlink('firstrun.php');
+	}
+} else {
+	echo "<p>It looks like some problems were found, please fix them then <input type=\"button\" value=\"reload\" onClick=\"window.location.reload()\"> the page.</p>";
+	echo "<p>If further assistance is needed, please visit the <a href='http://forum.xbmc.org/showthread.php?t=83304' target='_blank'>forum</a> or our <a href='http://mediafrontpage.lighthouseapp.com' target='_blank'>project page</a>.</p>";
+	echo "Attention WINDOWS users, please remember our WEB Server of choice for your platform is <a href='http://www.uniformserver.com/' target='_blank'>The Uniform Server</a>.";
 }
+	if(file_exists('default-config.php') || file_exists('config.php')){
+		$valid = true;
+		if(file_exists('config.php')){
+			$load = "config.php";
+		}else{
+			$load = "default-config.php";
+		}
+		if(isset($_POST['save_file']) && $_POST['save_file']) {
+			if(file_exists('config.php')){
+				$load = 'config.php';
+			}else{
+				if(copy("default-config.php", "config.php")){
+					$load = 'config.php';
+				}else{
+					$load = 'default-config.php';
+					$valid = false;
+				}
+			}
+			$savecontent = stripslashes($_POST['savecontent']);
+			$fp = @fopen($load, "w");
+			if ($fp) {
+				fwrite($fp, $savecontent);
+				fclose($fp);
+				if($valid){
+					echo "<script>alert('Configuration saved successfully');</script>";
+				}else{
+					echo "<script>alert('Unable to save configuration. Please open default-config.php in a text editor, fill in your information and save it as config.php.');</script>";
+				}
+				print '<a href='.$_SERVER['PHP_SELF'].'><input type="button" value="Refresh" /></a>';
+				//print "<html><head><META http-equiv=\"refresh\" content=\"0;URL='".$_SERVER['PHP_SELF']."'\"></head><body>";
+
+			}
+		}
+		$fp = @fopen($load, "r");
+		$loadcontent = fread($fp, filesize($load));
+		$lines = explode("\n", $loadcontent);
+		$count = count($lines);
+		$loadcontent = htmlspecialchars($loadcontent);
+		fclose($fp);
+		$line = '';
+		for ($a = 1; $a < $count+1; $a++) {
+			$line .= "$a\n";
+		}
 ?>
+<div id='config' style='display:none;'>
   <table class="widget" cellpadding=0 cellspacing=0 id=1>
     <tr style="cursor: move; ">
-      <td align=center colspan=2 height=25><div class="widget-head">MediaFrontPage One-Time Setup</div></td>
+      <td align=center colspan=2 height=25><div class="widget-head">MediaFrontPage One-Time Setup (<?php echo $load; ?>)</div></td>
     <tr>
-      <td align=right><form method=post action="<?=$_SERVER['PHP_SELF']?>">
+      <td align=right><form method=post action="<?php echo $_SERVER['PHP_SELF']?>">
           <table width="50%" valign="top" border="0" cellspacing="1" cellpadding="1">
             <tr>
-              <td width="50%" align="right" valign="top"><pre style="color:#FF9522; text-align: right; padding: 2px; overflow: auto; border: 0px groove; font-size: 10px" name="lines" cols="5" rows="<?=$count+3;?>"><?=$line;?>
+              <td width="50%" align="right" valign="top"><pre style="color:#FF9522; text-align: right; padding: 2px; overflow: auto; border: 0px groove; font-size: 10px" name="lines" cols="5" rows="<?php echo $count+10;?>"><?php echo $line;?>
 </pre></td>
-              <td width="100%" align="center" valign="top"><textarea style="border:1px solid #2C2D32; background:#2C2D32; color:#666666; text-align: left;  padding-left: 3px; overflow: auto; font-size: 10px" name="savecontent" cols="160" rows="<?=$count;?>" wrap="OFF"><?=$loadcontent?>
+              <td width="100%" align="center" valign="top"><textarea style="border:1px solid #2C2D32; background:#2C2D32; color:#666666; text-align: left;  padding-left: 3px; overflow: auto; font-size: 10px" name="savecontent" cols="160" rows="<?php echo $count;?>" wrap="OFF"><?php echo $loadcontent?>
 	</textarea></td>
             </tr>
           </table>
@@ -157,27 +208,11 @@ $line .= "$a\n";
         </form></td>
     </tr>
     </tr>
-    
+
   </table>
   <p>&nbsp;</p>
-
-<?
-
-//===============================================================
-// I got to here then confused myself. This is where 
-// I am lost - The 'Save' button needs to POST and then redirect. 
-// The redirect() is also commented out below for now
-// and firstrun deletion does not get reached yet.
-//===============================================================
-
-
-// 	echo "<script>setTimeout('redirect()', 5000);</script>";
-
-	if (file_exists('firstrun.php')){unlink('firstrun.php');}
-} else {
-	echo "<p>It looks like some problems were found, please fix them then <input type=\"button\" value=\"reload\" onClick=\"window.location.reload()\"> the page.</p>";
-	echo "<p>If further assistance is needed, please visit the <a href='http://forum.xbmc.org/showthread.php?t=83304' target='_blank'>forum</a> or our <a href='http://mediafrontpage.lighthouseapp.com' target='_blank'>project page</a>.</p>";
-	echo "Attention WINDOWS users, please remember our WEB Server of choice for your platform is <a href='http://www.uniformserver.com/' target='_blank'>The Uniform Server</a>.";
+</div>
+<?php
 }
 ?>
 </body>
