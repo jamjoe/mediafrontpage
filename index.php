ac3ec4c0c6fa45870b09fdbaccc72a4a793d860e
Gustavo Hoirisch <guga@maurorisch.com.br>
2011-06-10 05:41:03 -0700

    Modified redirection in index.php to javascript.

--- index.php
+++ index.php
@@ -1,24 +1,21 @@
 <?php
 //Check for config file and curl
 if (file_exists('firstrun.php')){header('Location: servercheck.php');exit;}
+//Authentication check
+require_once('config.php');
+if ($authsecured && (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin'])) {
+        echo "<script>window.location = 'login.php';</script>";
+	exit;
+}
 // Redirect if on a mobile browser
 require_once "m/mobile_device_detect.php";
-if( mobile_device_detect(true,true,true,true,true,true,true,false,false) ) {
+if(mobile_device_detect(true,true,true,true,true,true,true,false,false) ) {
   header('Location: m/');
   exit();
 }
 $submenu = false;
 require_once "config.php";
   if(!empty($subnavlink)||!empty($subnavlink_blank)||!empty($subnavselect)){$submenu = true;}
-?>
-<?php
-	//Authentication check
-	require_once('config.php');
-	if ($authsecured && (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin'])) 
-		{
-			header('Location: login.php');
-			exit;
-		}
 ?>
 <html>
   <head>
