2e01eca061a5365624b4e99cc8770916a4020455
gugahoi <guga@maurorisch.com.br>
2011-06-15 19:14:11 +1000

    Fixed ASCII heading formatting problem.

--- widgets/wSearch.php
+++ widgets/wSearch.php
@@ -4,24 +4,17 @@ $wIndex["wSearch"] = $wdgtSearch;
 
 function widgetSearch() {	
 	 echo <<<BODY
-            <div><input type="text"
-                  value=""
-                  id="searchterm"
-                  onkeydown="if (event.keyCode == 13) document.getElementById('searchbutton').click()">  <select id='provider' onchange="catDropDown(this.value);">
-                  <option value="0" selected>Default</option>
-                  <option value="1">nzb.su</option>
-                  <option value="2">NZB Matrix</option>
-                  <option value="3">TMDB</option>
-    			  </select> <select name="type" style="display:none;"
-                  id="type">
-            </select> <input type="button"
-                  id="searchbutton"
-                  value="Search"
-                  onclick="results()"> <input type="button"
-                  id="clearbutton"
-                  value="Clear"
-                  onclick="clearResults()"
-                  ondblclick="resetWidget()"></div>
+            <div><input type="text" value="" size="13" id="searchterm" onkeydown="if (event.keyCode == 13) document.getElementById('searchbutton').click()">  
+                  <select id='provider' onchange="catDropDown(this.value);">
+                    <option value="0" selected>Default</option>
+                    <option value="1" >nzb.su</option>
+                    <option value="2" >NZB Matrix</option>
+                    <option value="3" >TMDB</option>
+    			        </select>  
+    			        <input type="button" id="searchbutton" value="Find" onclick="results()">
+                  <input type="button" id="clearbutton" value="X" onclick="clearResults()" ondblclick="resetWidget()">
+                  <select name="type" style="display:none;" id="type"></select>
+           </div>
         <div id="resultstable"></div>
         <div id='extra_info'></div> 
 
